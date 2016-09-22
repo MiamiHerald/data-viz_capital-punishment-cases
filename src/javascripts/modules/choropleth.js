14fc432f090712c65c828936ee977a62b26d6636
Chris Williams <cwilliams@miamiherald.com>
2016-09-22 17:16:16 -0400

    Fixed responsiveness

--- src/javascripts/modules/choropleth.js
+++ src/javascripts/modules/choropleth.js
@@ -1,22 +1,23 @@
 import $ from 'jquery';
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
+import { TweenLite } from 'gsap';
 import numeral from 'numeral';
+window.$ = $;
 
 class Choropleth {
   constructor(el, dataUrl) {
     this.el = el;
+    this.aspectRatio = 0.6667;
+    this.width = $(this.el).width();
+    this.height = Math.ceil(this.aspectRatio * this.width);
+    this.mapWidth = this.width;
     this.dataUrl = dataUrl;
     this.shapeUrl = `/data/new-florida.json`;
     this.rateById = d3.map();
     this.quantize = d3.scaleQuantize()
       .domain([10000, 60000])
       .range(d3.range(4).map((i) => `median--${i + 1}`));
-    this.projection = d3.geoMercator()
-      .center([-81.5158, 27.6648])
-      .scale(3500);
-    this.path = d3.geoPath()
-      .projection(this.projection);
   }
 
   render() {
@@ -27,12 +28,19 @@ class Choropleth {
 
     this.loadData();
     this.resizeChoropleth();
-    $(window).on(`resize`, this.resizeChoropleth.bind(this))
+    $(window).on(`resize`, this.resizeChoropleth.bind(this));
   }
 
   resizeChoropleth() {
-    d3.select(`g`).attr(`transform`, `scale(${$(this.el).width() / 900})`);
-    $(`.choropleth__svg`).height($(this.el).width() * 0.618);
+    window.requestAnimationFrame(() => {
+      const chart = $(this.el).find(`g`);
+
+      this.width = $(this.el).width();
+      this.height = Math.ceil(this.aspectRatio * this.width);
+
+      TweenLite.set(chart, { scale: this.width / this.mapWidth });
+      d3.select(`.choropleth__svg`).attr(`height`, this.height);
+    });
   }
 
   loadData() {
@@ -44,12 +52,15 @@ class Choropleth {
   drawMap(error, shapeData, caseData) {
     if (error) throw error;
 
-    this.svg.append(`g`)
-        .attr(`class`, `counties`)
-      .selectAll(`path`)
+    this.projection = d3.geoMercator()
+      .fitSize([this.width, this.height], topojson.feature(shapeData, shapeData.objects[`cb_2015_florida_county_20m`]));
+    this.path = d3.geoPath()
+      .projection(this.projection);
+
+    this.svg.selectAll(`path`)
         .data(topojson.feature(shapeData, shapeData.objects.cb_2015_florida_county_20m).features)
       .enter().append(`path`)
-        .attr(`class`, (d) => `${this.quantize(numeral().unformat(d.properties.Median))} circuit--${d.properties.CIRCUIT} county--${d.properties.NAME}`)
+        .attr(`class`, (d) => `${this.quantize(numeral().unformat(d.properties.Median))} circuit--${d.properties.CIRCUIT} county`)
         .attr(`d`, this.path)
         .on(`mouseover`, (d) => {
           d3.select(`#info`)
